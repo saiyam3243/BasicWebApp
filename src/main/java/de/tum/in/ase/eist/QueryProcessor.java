dae6df074043ad0f84b677b5c2892f51510e3dfe
ge63req <sjain3243@gmail.com>
2022-07-14 15:03:22 +0200

    w

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -14,7 +14,7 @@ public class QueryProcessor {
         } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
-            return "Saiyam Jain";
+            return "saiyam jain";
         }
     }
 }
