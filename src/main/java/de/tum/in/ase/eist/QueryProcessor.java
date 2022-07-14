d3964098ec328147d5ac5cfc7d501f4c8edc8fd8
ge63req <sjain3243@gmail.com>
2022-07-14 15:19:03 +0200

    w

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("saiyam jain")) {
-           return "MyTeam";
+           return "saiyam";
         } else { // TODO extend the programm here
             return "";
         }
