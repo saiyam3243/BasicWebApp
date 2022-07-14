6c32c2d9799ab705b91e2414a04366ed10bf9f2b
ge63req <sjain3243@gmail.com>
2022-07-14 15:00:48 +0200

    w

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,10 +11,10 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("Saiyam jain")) {
+        } else if (query.contains("name")) {
            return "MyTeam";
         } else { // TODO extend the programm here
-            return "";
+            return "Saiyam Jain";
         }
     }
 }
